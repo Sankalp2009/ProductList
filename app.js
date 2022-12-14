6571af9e2e42f4e4f0975d37213e6cbc0c56f912
Sankalp <sankalppatel38@gmail.com>
2022-12-14 23:44:17 +0530

    ProductListApi

---
+++ app.js
@@ -0,0 +1,22 @@
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const ProductRouter = require('./routes/ProductRoutes')
+
+app.use(express.json()); // JSON Middleware 
+app.use(cors({
+    origin : "*"
+}));
+
+app.use('/api/v1/Products',ProductRouter); 
+
+app.all("*",(req,res,next)=>{
+    res.status(400).json({
+        status : "Fail",
+        message : "Invalid request"
+
+    })
+    next();
+})
+module.exports = app;
